76d558dde9e499294125ecf4512d33f68ec3cb32
Jordan <jordanrobbedthesun@gmail.com>
2024-09-28 10:59:37 -0400

    Initialized front and back end

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+     res.send('API is running');
+});
+
+// Start the server
+app.listen(port, () => {
+     console.log(`Server running on port ${port}`);
+});
